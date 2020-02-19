fba0093b0bd3562f0b759e271bfab186d7636622
arieldavis22 <aadavisbusiness@gmail.com>
2020-02-19 18:04:07 -0500

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,30 @@
 # code here!
+require 'pry'
+class School 
+    
+    attr_accessor :roster
+    def initialize(name,roster={})
+        @name = name
+        @roster = roster
+    end
+
+    def add_student(name, grade)
+        if !@roster[grade]
+            @roster[grade] = []
+            @roster[grade] << name
+        else
+            @roster[grade] << name
+        end
+    end
+
+    def grade(grade)
+        @roster[grade]
+    end
+
+    def sort
+        @roster.map do |k, v|
+            v.sort!
+        end
+        @roster
+    end
+end
